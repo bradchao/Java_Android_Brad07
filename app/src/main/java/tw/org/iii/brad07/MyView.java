5a7348695f82a90a502b455df2efef4351b8566e
brad <brad@brad.tw>
2017-01-15 13:08:45 +0800

    v6

--- app/src/main/java/tw/org/iii/brad07/MyView.java
+++ app/src/main/java/tw/org/iii/brad07/MyView.java
@@ -3,6 +3,7 @@ package tw.org.iii.brad07;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -32,5 +33,9 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        paint.setStrokeWidth(4);
+        canvas.drawLine(0,0,200,200,paint);
     }
 }
